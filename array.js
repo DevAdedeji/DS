b565926ba6a53c8d1b2640784a560d1e19d1da88
DevAdedeji <adedejitewogbade2@gmail.com>
2022-04-24 23:00:16 +0100

    Some array methods

--- array.js
+++ array.js
@@ -34,6 +34,38 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // we can also use the slice method to insert numbers into an array
 numbers = [1, 5, 6, 7, 8, 9, 10]
 numbers.splice(1, 0, 2, 3, 4)
+// console.log(numbers);
 
+// Other common methods
+// Concat: To join multiple arrays and returns a copy of the joined arrays
+let array1 = [1, 2, 3]
+let array2 = [4, 5, 6]
+let newArray = array1.concat(array2)
+// console.log(newArray);
+// every: Iterates every element of the array, verifying the desired condition untill the condition is false, it returns a boolean value
+let ages = [22, 18, 12, 23, 15, 18, 90, 14]
+function checkAge(age) {
+    return age > 10;
+}
+// console.log(ages.every(checkAge));
 
-console.log(numbers);
+// forEach method: executes a specific function on each element of an array
+let nums = [1, 2, 3, 4, 5]
+nums.forEach((num) => {
+    // console.log(num * 2);
+})
+
+// Map method creates a new array from a function that is called on each element in that array
+let newNums = nums.map((num) => {
+    return num * 2;
+})
+// console.log(newNums);
+
+// Join method: Used to join all elements in an array into a string
+
+let words = ["My", "name", "is", "Tewogbade", "Adedeji"]
+let sentence = words.join(' ')
+// console.log(sentence);
+
+// toSring: Returns an array as string
+console.log(words.toString());
--- array2.js
+++ array2.js
@@ -1,1 +1,17 @@
 // TWO DIMENSIONAL AND MULTI-DIMENSIONAL ARRAYS
+// A matrix is a two dimensional array
+let averageTemp = [];
+averageTemp[0] = [72, 75, 79, 79, 81, 81];
+averageTemp[1] = [81, 79, 75, 75, 73, 73];
+
+// A function that iterates a two dimesional array
+let printMatrix = (matrix) => {
+    for (let i = 0; i < matrix.length; i++) {
+        let newMatrix = matrix[i];
+        for (let j = 0; j < newMatrix.length; j++) {
+            console.log(newMatrix[j]);
+        }
+    }
+}
+
+printMatrix(averageTemp)
