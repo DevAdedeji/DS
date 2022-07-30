442aa2b877bc7a105df81ebe7a4f649e30d1a89f
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-30 12:42:12 +0100

    Leaarnt about linear time and constant time

--- Big0/1.js
+++ Big0/1.js
@@ -1,24 +1,17 @@
-// const nemo = ['nemo'];
-// const everyone = ['Adedeji', 'Demilade', 'Wale', 'Tunde', 'nemo', 'Ijaya'];
-// const large = new Array(10000).fill('nemo')
+const nemo = ['nemo'];
+const everyone = ['Adedeji', 'Demilade', 'Wale', 'Tunde', 'nemo', 'Ijaya'];
+const large = new Array(10000).fill('nemo')
 
-// function findNemo(array){
+function findNemo(array){
     
-//     for(let i = 0; i < array.length; i++){
-//         if(array[i] === 'nemo'){
-//             console.log("Nemo Found");
-//         }
-//     }
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 'nemo'){
+            console.log("Nemo Found");
+            break;
+        }
+    }
     
-// }
-
-// findNemo(large); // O(n)
-
-const boxes = [0,1,2,3,4,5]
-
-function logFirstTwoBoxes(boxes){
-    console.log(boxes[0]); // O(1)
-    console.log(boxes[1]); // O(1)
 }
 
-logFirstTwoBoxes(boxes); // O(2)
+findNemo(everyone); // O(n)
+
