11e7ca1d7b090174c5bd658875410c4bb31c77d1
DevAdedeji <adedejitewogbade2@gmail.com>
2022-07-30 12:34:52 +0100

    Learnt how to calculate Big O

--- Big0/1.js
+++ Big0/1.js
@@ -1,15 +1,24 @@
-const nemo = ['nemo'];
-const everyone = ['Adedeji', 'Demilade', 'Wale', 'Tunde', 'nemo', 'Ijaya']
-function findNemo(array){
-    let t0 = performance.now();
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === 'nemo'){
-            console.log("Nemo Found");
-        }
-    }
-    let t1 = performance.now();
+// const nemo = ['nemo'];
+// const everyone = ['Adedeji', 'Demilade', 'Wale', 'Tunde', 'nemo', 'Ijaya'];
+// const large = new Array(10000).fill('nemo')
 
-    console.log('Call to find nemo took', (t1-t0), 'milliseconds');
+// function findNemo(array){
+    
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] === 'nemo'){
+//             console.log("Nemo Found");
+//         }
+//     }
+    
+// }
+
+// findNemo(large); // O(n)
+
+const boxes = [0,1,2,3,4,5]
+
+function logFirstTwoBoxes(boxes){
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
 }
 
-findNemo(everyone);
+logFirstTwoBoxes(boxes); // O(2)
